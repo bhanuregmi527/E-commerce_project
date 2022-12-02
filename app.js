f796fa718e43389e26dfc5d129e7e7c66d58cdae
bhanu527 <regmibhanubhakta67@gmail.com>
2022-12-02 16:09:30 +0545

    User_schema Completed

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express= require('express')
 require('dotenv').config()
 const mongoose=require('mongoose')
+const morgan= require('morgan')
+const bodyParser= require('body-parser')
+const cookieParser= require('cookie-parser')
 // import routes
 const userRoutes=require('./routes/user')
+
 
 //apps
 const app= express()
@@ -11,6 +15,13 @@ const app= express()
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser:true,
     }).then(()=> console.log(`DB connected `))
+
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
 
 
 //routes middleware
